b1748134f8636803b20304341489fe3d72516c74
Arham Naveed <arham.king98@gmail.com>
2024-11-03 21:42:16 +0500

    update contacts

--- src/pages/Frontend/Contact/Contact.js
+++ src/pages/Frontend/Contact/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     return (
         <>
             <h1 className="text-center py-5 ">Welcome To Contact Page</h1>
-            <h1 className="text-center py-5">Owner's Email : inamabdullah58@gmail.com  </h1>
+            <h1 className="text-center py-5">Owner's Email : arham.king98@gmail.com  </h1>
             <h1 className="text-center py-5">Owner's Phone # +92 3177408609  </h1>
         </>
     )
